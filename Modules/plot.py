f41f1833e1d725daf157adf982d9072fb465311e
Ramprakash Chandrashekar <ramprakash.chandrashekar@gmail.com>
2020-06-14 02:04:27 +0530

    style: Tabs for indents

--- Modules/plot.py
+++ Modules/plot.py
@@ -6,16 +6,18 @@ import numpy as np
 import sys
 from pathlib import Path
 
-figPath = ""
+figPath = ''
 
 if(len(sys.argv) == 1):
-    figPath = "../img/fig.png"
+	figPath = 'img/fig.png'
 elif(len(sys.argv) > 2):
-    print("Too many arguments")
+	print("Too many arguments")
 else:
-    figPath = sys.argv[1]
+	figPath = sys.argv[1]
 
 figFileName = Path(figPath)
+print('Image saved to-')
+print(figFileName)
 
 p1 = projectile.IdealProjectile(30, 45)
 p2 = projectile.ProjectileInMedium(30, 45, 0.450, 0.22)
@@ -27,13 +29,13 @@ yList2 = []
 
 for t in np.arange(0, p1.idealFlightTime, 0.05):
 
-    xList1.append(p1.Inst_Param_Ideal(t)[0])
-    yList1.append(p1.Inst_Param_Ideal(t)[1])
+	xList1.append(p1.Inst_Param_Ideal(t)[0])
+	yList1.append(p1.Inst_Param_Ideal(t)[1])
 
 for t in np.arange(0, p2.realFlightTime, 0.05):
 
-    xList2.append(p2.Inst_Param_Resistance(t)[0])
-    yList2.append(p2.Inst_Param_Resistance(t)[1])
+	xList2.append(p2.Inst_Param_Resistance(t)[0])
+	yList2.append(p2.Inst_Param_Resistance(t)[1])
 
 # Plotting using pyplot
 plt.figure(1)
